1e57ca459d8f8683d55a7c6e2b14af672641cf55
Jadem Paulino Gomez <jaypaulino234@gmail.com>
2018-11-11 15:23:34 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,14 +1,6 @@
 # Define the variable board below.
 board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-# position 1: left 
-row[0]
 
-#position 2: middle 
-row[1]
-
-# position 3: right
-row[2]
-
-#move x to the position 2, middle
-row[1] = "x"
-row #=> [" ", " x", " ",]
+" "|" "|" "
+" "|" "|" "
+" "|" "|" "
